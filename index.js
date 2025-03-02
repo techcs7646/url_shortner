ea55da7ff01b9081baf63941f70c0890cfc8626e
Chandrakanta Satapathy <114939097+techcs7646@users.noreply.github.com>
2025-03-02 21:39:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // Load environment variables
+
 const express = require('express');
 const mongoose = require('mongoose');
 const urlRoutes = require('./routes/urlRoutes');
@@ -8,8 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/urlshortener')
+// Connect to MongoDB securely
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
